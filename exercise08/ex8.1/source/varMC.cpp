88009e33427258156afb6270530c018b06f2c172
angelariva <angelariva@icloud.com>
2021-06-10 12:27:06 +0200

    finished exercise08!

--- exercise08/ex8.1/source/varMC.cpp
+++ exercise08/ex8.1/source/varMC.cpp
@@ -43,55 +43,6 @@ void Particle::Metropolis() {
     tot++;
 }
 
-/*
-void Particle::Optimize(double mu_start,
-                        double mu_var,
-                        double sigma_start,
-                        double sigma_var,
-                        double x_start,
-                        double x_j,
-                        int n_samples,
-                        int N_tot,
-                        int iter)
-                        {
-  double mu_min, mu_delta, mu_trial, mu_tmp=0.;
-  double sigma_min, sigma_delta, sigma_trial;
-  double integral = 1E10;
-  double integral_old = 0.;
-
-  mu_min = mu_start;
-  sigma_min = sigma_start;
-
-  for(int i=0; i<iter; i++){
-    sigma_delta = 2.*sigma_var/double(n_samples*pow(10., i));
-    sigma_trial = sigma_min - sigma_var/pow(10., i);
-    mu_delta = 2.*mu_var/double(n_samples*pow(10., i));
-    mu_trial = mu_min - mu_var/pow(10., i);
-    mu_tmp = mu_trial;
-
-    for(int k=0; k<n_samples; k++) {
-            for(int z=0; z<n_samples; z++) {
-                Reset(mu_trial, sigma_trial, x_start, x_j);
-                for(int j=0; j<N_tot; j++) integral += Sample();
-                integral /= N_tot;
-                if(integral < integral_old) {
-                    mu_min = mu_trial;
-                    sigma_min = sigma_trial;
-                }
-                integral_old = integral;
-                integral = 0.;
-                mu_trial += mu_delta;
-            }
-            mu_trial = mu_tmp;
-            sigma_trial += sigma_delta;
-        }
-
-  }
-  mu_opt = mu_min;
-  sigma_opt = sigma_min;
-}
-*/
-
 double Particle::EnergyLoc(double a) {
   double s, T, V;
   s = sigma*sigma;
